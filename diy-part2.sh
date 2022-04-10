def9c5b0e662fdfe6f3e08086a1583d038a613f0
alois11 <85094874+alois11@users.noreply.github.com>
2022-04-10 12:41:50 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,8 +12,8 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
 # hostname
-sed -i 's/OpenWrt/MiNano_R1CL/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/MiNano/g' package/base-files/files/bin/config_generate
 # wifiname
 sed -i 's/OpenWrt/MiNano/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 # theme
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
